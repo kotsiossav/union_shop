e756299a3b5ce6ab0e9a41d2c898ff1955ae4b43
kotsiossav <kotsios.savva9@gmail.com>
2025-12-05 08:08:39 +0000

    Remove unnecessary import of main.dart in layout.dart for cleaner code structure.

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
-import 'main.dart';
+
 import 'services/auth_service.dart';
 import 'models/cart_model.dart';
 
